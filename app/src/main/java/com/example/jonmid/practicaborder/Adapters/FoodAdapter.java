2bbbf1ff067d933bff3f830751ccfcc4c1386c27
pc713 <pc713@iucesmag.edu.co>
2018-04-26 13:29:48 -0500

    5

--- app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
@@ -1,13 +1,20 @@
 package com.example.jonmid.practicaborder.Adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.jonmid.practicaborder.Models.Food;
+import com.example.jonmid.practicaborder.R;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class FoodAdapter {
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
 
     List<Food> foodList = new ArrayList<>();
     Context context;
@@ -15,5 +22,32 @@ public class FoodAdapter {
     public FoodAdapter(List<Food> foodList, Context context){
         this.foodList = foodList;
         this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_food, parent, false);
+        ViewHolder viewHolder = new ViewHolder(item);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.textViewFood.setText(foodList.get(position).getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return foodList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textViewFood;
+
+        public ViewHolder(View item) {
+            super(item);
+        }
     }
 }
--- app/src/main/java/com/example/jonmid/practicaborder/Parser/JsonFood.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Parser/JsonFood.java
@@ -1,4 +1,31 @@
 package com.example.jonmid.practicaborder.Parser;
 
+import com.example.jonmid.practicaborder.Models.Food;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonFood {
+
+    public static List<Food> getData(String content) throws JSONException {
+
+        JSONObject jsonObject = new JSONObject(content);
+        JSONArray jsonArray = jsonObject.getJSONArray("results");
+
+        List<Food> foodList = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject item = jsonArray.getJSONObject(i);
+
+            Food food = new Food();
+            food.setTitle(item.getString("title"));
+
+            foodList.add(food);
+        }
+        return foodList;
+    }
 }
