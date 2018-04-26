cc88e1917dd96538c9f79aeffcdf495897e4898d
pc713 <pc713@iucesmag.edu.co>
2018-04-26 12:42:43 -0500

    4

--- app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
@@ -1,4 +1,19 @@
 package com.example.jonmid.practicaborder.Adapters;
 
+import android.content.Context;
+
+import com.example.jonmid.practicaborder.Models.Food;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodAdapter {
+
+    List<Food> foodList = new ArrayList<>();
+    Context context;
+
+    public FoodAdapter(List<Food> foodList, Context context){
+        this.foodList = foodList;
+        this.context = context;
+    }
 }
